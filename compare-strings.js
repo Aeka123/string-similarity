c6c665b92d3dde61819d9b9d4cc9d3b7d2b36af1
Esben Volsgaard <26848437+Aeka123@users.noreply.github.com>
2019-04-24 16:22:49 +0200

    Promise based findBestMatch

--- compare-strings.js
+++ compare-strings.js
@@ -40,24 +40,29 @@ function compareTwoStrings(first, second) {
 }
 
 function findBestMatch(mainString, targetStrings) {
-	if (!areArgsValid(mainString, targetStrings)) throw new Error('Bad arguments: First argument should be a string, second should be an array of strings');
-	
-	const ratings = [];
-	let bestMatchIndex = 0;
-
-	for (let i = 0; i < targetStrings.length; i++) {
-		const currentTargetString = targetStrings[i];
-		const currentRating = compareTwoStrings(mainString, currentTargetString)
-		ratings.push({target: currentTargetString, rating: currentRating})
-		if (currentRating > ratings[bestMatchIndex].rating) {
-			bestMatchIndex = i
+	return new Promise(function(resolve, reject) {
+		if (!areArgsValid(mainString, targetStrings)) {
+			reject(new Error('Bad arguments: First argument should be a string, second should be an array of strings'));
+			//throw new Error('Bad arguments: First argument should be a string, second should be an array of strings');
 		}
-	}
-	
-	
-	const bestMatch = ratings[bestMatchIndex]
-	
-	return { ratings, bestMatch, bestMatchIndex };
+		const ratings = [];
+		let bestMatchIndex = 0;
+
+		for (let i = 0; i < targetStrings.length; i++) {
+			const currentTargetString = targetStrings[i];
+			const currentRating = compareTwoStrings(mainString, currentTargetString)
+			ratings.push({target: currentTargetString, rating: currentRating})
+			if (currentRating > ratings[bestMatchIndex].rating) {
+				bestMatchIndex = i
+			}
+		}
+		
+		
+		const bestMatch = ratings[bestMatchIndex]
+		
+		// return { ratings, bestMatch, bestMatchIndex };
+		resolve( { ratings, bestMatch, bestMatchIndex } );
+	})
 }
 
 function flattenDeep(arr) {
